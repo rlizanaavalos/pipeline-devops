53968c18afcee10368d44b252d4b8fb2338c721a
Rodrigo L <r.lizanaavalos@gmail.com>
2022-01-26 21:36:08 -0300

    ejecucion.groovy

---
+++ vars/ejecucion.groovy
@@ -0,0 +1,15 @@
+/*
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+*/
+
+def call(){
+  
+  stage(){
+    //Escribir directamente el código del stage, sin agregarle otra clausula de Jenkins.
+  }
+
+}
+
+return this;
